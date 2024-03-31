d35f56c3255c6d3132e0b16568fb86487fb190c5
dino.priyano <dino.priyano@gdn-commerce.com>
2024-03-31 20:54:31 +0700

    :hammer: custom kamel coroutine context

--- composeApp/src/commonMain/kotlin/com/dino/newskmp/designSystem/presentation/component/Image.kt
+++ composeApp/src/commonMain/kotlin/com/dino/newskmp/designSystem/presentation/component/Image.kt
@@ -5,8 +5,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import com.dino.newskmp.platform.ioDispatcher
+import io.kamel.core.utils.cacheControl
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
+import io.ktor.client.utils.CacheControl
+import kotlinx.coroutines.Job
 
 /**
  * Created by dinopriyano on 29/03/24.
@@ -21,7 +25,12 @@ fun RawrImage(
 ) {
     when {
         (data is String && data.contains("http")) -> {
-            val resource = asyncPainterResource(data)
+            val resource = asyncPainterResource(data) {
+                coroutineContext = Job() + ioDispatcher
+                requestBuilder {
+                    cacheControl(CacheControl.MAX_AGE)
+                }
+            }
             KamelImage(
                 resource = resource,
                 modifier = modifier,
